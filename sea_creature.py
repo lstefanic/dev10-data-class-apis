f63505f2c7cd37e6bae43bb99e068645713755a7
Lucas Stefanic <lucas.stefanic@zoho.com>
2022-01-06 11:42:05 -0600

    Wrote code to scrape all sea creatures

--- sea_creature.py
+++ sea_creature.py
@@ -1,7 +1,8 @@
 from dataclasses import dataclass, field
+import requests
 
 @dataclass
-class Sea_Creatures:
+class Sea_Creature:
     id: int
     name: dict
     availability: dict
@@ -10,3 +11,23 @@ class Sea_Creatures:
     price: int
     catch_phrase: str
     museum_phrase: str
+
+def createSeaCreature(creature_dict):
+    id = creature_dict["id"]
+    name = creature_dict["name"]
+    availability = creature_dict["availability"]
+    speed = creature_dict["speed"]
+    shadow = creature_dict["shadow"]
+    price = creature_dict["price"]
+    catch_phrase = creature_dict["catch-phrase"]
+    museum_phrase = creature_dict["museum-phrase"]
+    return Sea_Creature(id,name,availability,speed,shadow,price,catch_phrase,museum_phrase)
+
+def createSeaCreatures(creatures_dict):
+    return map(createSeaCreature,list(creatures_dict.values()))
+
+response = requests.get("http://acnhapi.com/v1/sea")
+print(response.status_code)
+creatures = createSeaCreatures(response.json())
+for creature in creatures:
+    print(creature)

4bf822c20f7c5472600111b428a2756b8d3fe957
Lucas Stefanic <lucas.stefanic@zoho.com>
2022-01-06 11:48:53 -0600

    Wrote code to scrape all fossils

--- fossil.py
+++ fossil.py
@@ -1,7 +1,23 @@
 from dataclasses import dataclass, field
+import requests
 
 @dataclass
-class Fossils:
+class Fossil:
     name: dict
     price: int
     museum_phrase: str
+
+def createFossil(fossil_dict):
+    name = fossil_dict["name"]
+    price = fossil_dict["price"]
+    museum_phrase = fossil_dict["museum-phrase"]
+    return Fossil(name,price,museum_phrase)
+
+def createFossils(fossils_dict):
+    return map(createFossil,list(fossils_dict.values()))
+
+response = requests.get("http://acnhapi.com/v1/fossils")
+print(response.status_code)
+fossils = createFossils(response.json())
+for fossil in fossils:
+    print(fossil)

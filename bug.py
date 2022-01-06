599287655f324acb2effc6274f202babe11b6407
gregory-wagner <94394082+gregory-wagner@users.noreply.github.com>
2022-01-06 12:48:11 -0500

    Update bug.py
    Applied code from villager.py to get all bugs from API

--- bug.py
+++ bug.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass, field
+import requests
 
 @dataclass
 class Bugs:
@@ -9,3 +10,22 @@ class Bugs:
     price_flick: int
     catch_phrase: str
     museum_phrase: str
+
+def createBug(bug_dict):
+    id = bug_dict["id"]
+    name = bug_dict["name"]
+    availability = bug_dict["availability"]
+    price = bug_dict["price"]
+    price_flick = bug_dict["price-flick"]
+    catch_phrase = bug_dict["catch-phrase"]
+    museum_phrase = bug_dict["museum-phrase"]
+    return Bugs(id,name,availability,price,price_flick,catch_phrase,museum_phrase)
+
+def createBugs(bugs_dict):
+    return map(createBug,list(bugs_dict.values()))
+
+response = requests.get("http://acnhapi.com/v1/bugs")
+print(response.status_code)
+bugs = createBugs(response.json())
+for bug in bugs:
+    print(bug)        
